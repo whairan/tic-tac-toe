6b6da4c1d82cce1f4c1206af591c52cd075405a1
whairan <waliodysseus@gmail.com>
2025-09-01 19:08:12 -0400

    change local file names.

---
+++ main.py
@@ -0,0 +1,16 @@
+
+evl = ""
+
+for i in range(3):
+    print("-----------------")
+    print(f"| {evl}  |  {evl}  |  {evl} |")
+
+
+
+inp = input()
+
+if inp != "o" or "x":
+    print("Bruh, just use 'x' or 'o'. No other input is accepted.")
+# if inp == "o":
+
+#once input is taken, print the input inside of the 
